15e8b6a79d71430d640a2faf242ba7edcd807bac
John Tribolet <john@tribolet.info>
2016-08-08 20:07:24 -0400

    IndieGala Automation - Switch to Nightmare for initial parse, gets first 2 pages of giveaway links.

--- giveaways.js
+++ giveaways.js
@@ -12,43 +12,59 @@
 
 'use strict';
 
+const Nightmare = require('nightmare');
 const request    = require('request');
 const cheerio    = require('cheerio');
 const sqlite3    = require('sqlite3').verbose();
 
 const baseUrl     = 'https://www.indiegala.com';
 const giveawayUrl = baseUrl + '/giveaways';
+
+const chromeUA    = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36';
 // const db          = new sqlite3.Database('db.sqlite3');
 
+const nightmareConfig = {
+    show: true,
+    fullscreen: false
+};
 
 // setup sqlite3 db
 
 
 
 // parse games
-let options = {
-    'url': giveawayUrl,
-    'jar': true,
-    'headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-    }
-}
-request(options, (err, resp, body) => {
-    if (err) {
-        console.error('Error loading main page ' + giveawayUrl + '\n ' + err);
-        return;
-    }
-    const $ = cheerio.load(body);
-    console.log(resp.statusCode);
-    console.log(body);
-    let games = [];
-    $('.ticket-info-cont h2 a').each((i, elem) => {
-        let url = $(elem).attr('href');
-        console.log(url);
-        if (-1 === games.indexOf(url)) {
-            games.push(url);
-        }
-    });
+const nextSelector = '.page-nav > div:nth-child(7) > a';
+let nmInst = Nightmare(nightmareConfig);
 
-    console.log(games);
-});
+function parseGameLinks() {
+    var links = document.querySelectorAll('.ticket-info-cont h2 a');
+
+    return Array.prototype.map.call(links, function (e) {
+        return e.getAttribute('href');
+    });
+}
+
+nmInst
+    .goto(giveawayUrl)
+    .wait('.giveaways-main-page')
+    .evaluate(function() {
+        var links = document.querySelectorAll('.ticket-info-cont h2 a');
+
+        return Array.prototype.map.call(links, function (e) {
+            return e.getAttribute('href');
+        });
+    })
+    .then((links) => {
+        console.log(links);
+    })
+    .then(() => {
+        return nmInst
+            .click(nextSelector)
+            .evaluate(parseGameLinks)
+    })
+    .then((links) => {
+        console.log('Page 2 Links');
+        console.log(links);
+    })
+    .catch((err) => console.error('Error on main page: ' + error));
+
