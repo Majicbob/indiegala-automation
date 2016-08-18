3eeee1cb69ea35d2509e97600fc4da1daf0a2c1d
John Tribolet <john@tribolet.info>
2016-08-17 20:38:36 -0400

    Add table creation and insertGiveaway queries to model

--- model.js
+++ model.js
@@ -6,17 +6,64 @@
  */
 
 'use strict';
+const nconf       = require('./config');
 const sqlite3     = require('sqlite3').verbose();
-const db          = new sqlite3.Database('db.sqlite3');
+const db          = new sqlite3.Database(nconf.get('sqliteFile'));
 
 // setup tables
+const createGamesTableSql = `
+    CREATE TABLE IF NOT EXISTS games (
+        steamId INTEGER,
+        reviewText TEXT,
+        reviewStats TEXT,
+        genre TEXT,
+        metascore INTEGER,
+        tag1 TEXT,
+        tag2 TEXT,
+        tag3 TEXT,
+        shortDesc TEXT,
+        PRIMARY KEY(steamId)
+    );
+`;
 
+const creatGiveawaysTableSql = `
+    CREATE TABLE IF NOT EXISTS giveaways (
+        id INTEGER,
+        name TEXT,
+        steamUrl TEXT,
+        price NUMERIC,
+        endDate INTEGER,
+        steamId INTEGER,
+        entered INTEGER DEFAULT 0,
+        PRIMARY KEY(id)
+    );
+`;
+
+db.parallelize(() => {
+    db.run(createGamesTableSql);
+    db.run(creatGiveawaysTableSql);
+});
 
 // setup queries
+
+// giveaways queries
+const insertGiveaway = db.prepare(
+        'INSERT INTO giveaways (id, name, steamUrl, price, endDate, steamId) ' +
+        'VALUES (?, ?, ?, ?, ?, ?)');
+
 const markAsEntered = db.prepare('UPDATE giveaways SET entered = 1 WHERE id = ?');
+
+// games queries
+const insertGame    = db.prepare('INSERT INTO games VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+
+
 
 module.exports = {
 
+    // direct interface to the db
+    db: db,
+
+    // set giveaways.entered = 1 for the provided id
     markAsEntered: (giveawayId) => {
         markAsEntered.run(giveawayId, (updateError) => {
             if (updateError) {
@@ -26,5 +73,27 @@ module.exports = {
                 console.log('Updated ' + giveawayId);
             }
         });
+    },
+
+    // insert new row to giveaways
+    insertGiveaway: (giveaway) => {
+        insertGiveaway.run(
+            giveaway.id,
+            giveaway.name,
+            giveaway.steamUrl,
+            giveaway.price,
+            giveaway.endDate,
+            giveaway.steamId,
+            (insertErr) => {
+                if (insertErr) {
+                    // ignore PK insert errors
+                    if (! insertErr.message.includes('UNIQUE constraint failed')) {
+                        console.error(insertErr);
+                    }
+                }
+            }
+        );
     }
+
+    // insertGame:
 }
