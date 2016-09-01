f29a225bf4e28cee6a7c9c2617409cd068836158
John Tribolet <john@tribolet.info>
2016-09-01 06:36:05 -0400

    Index file for tieing all modules together now that they are done enough to be used that way

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Automation of IndieGala Giveaways
+ *
+ * Overview:
+ * Parse giveaways and collect database of games with info from Steam
+ * Allow configuration of priorities for games, based on name, genre, etc
+ * Use Nightmare to login and enter the Giveaways (JS is required for clicking on the ticket) *
+ *
+ * @author   John Tribolet <john@tribolet.info>
+ * @created  2016-09-01 03:02
+ */
+
+'use strict';
+
+
+const ig      = require('./indiegala');
+const steam   = require('./steam');
+
+steam.updateOwnedGames();
+
+ig.parseGiveawaysList()
+    .then(steam.scrape);
