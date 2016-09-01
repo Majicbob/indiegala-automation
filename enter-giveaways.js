c6e694735e98e2fb51e7f5cf24906ecb987c66fc
John Tribolet <john@tribolet.info>
2016-09-01 13:02:20 -0400

    Export prioritizeGiveaways function and clean up

--- enter-giveaways.js
+++ enter-giveaways.js
@@ -12,34 +12,33 @@
 
 'use strict';
 
-// modules
-const nconf   = require('./config');
 const model   = require('./model');
-const ig      = require('./indiegala');
 const Q       = require('q');
 
 /**
  * Read options/preferences (to be implemented) and prioritize games accordingly
  *
- * @promise {String[]}  Prioritized array of giveaway URLs, highest priority in 0 index
+ * @promise {Object[]}
+ * @return {Promise} Object[]  Obj with url and id, highest priority is 0 index
  */
 function prioritizeGiveaways() {
     const deferred = Q.defer();
 
     // games with positive in the reviewText sorted by endDate
     const now = Math.floor(new Date() / 1000);
-    const sql =
-        'SELECT id FROM giveaways ga ' +
-        'INNER JOIN games g on ga.steamId = g.steamId ' +
-        'WHERE entered = 0 AND endDate > ' + now + ' ' +
-        'AND ( ' +
-        '    reviewText like "%Positive" ' +
-        '    OR metascore >= 70 ' +
-        ') ' +
-        'AND price <= 20 ' +
-        'AND name != "Particula" ' +
-        'ORDER BY endDate ASC ' +
-        'LIMIT 30';
+    const sql = `
+        SELECT id FROM giveaways ga
+        INNER JOIN games g on ga.steamId = g.steamId
+        WHERE entered = 0 AND endDate > ${now}
+        AND (
+            reviewText like "%Positive"
+            OR metascore >= 70
+        )
+        AND price <= 20
+        AND name != "Particula"
+        ORDER BY endDate ASC
+        LIMIT 30;
+    `;
 
 
     model.db.all(sql, (err, rows) => {
@@ -61,11 +60,6 @@ function prioritizeGiveaways() {
 }
 
 
-prioritizeGiveaways()
-    .then( ig.enterGiveaways )
-    .then( ig.checkWins )
-    .then( ig.close )
-    .catch( (err) => {
-        console.error(err);
-        process.exit();
-    });
+module.exports = {
+    prioritizeGiveaways
+};
